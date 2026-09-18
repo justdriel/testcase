32c77b6950c406f633c7cdbdc8989dba24fddcea
Somto Eboh <adrelspark@gmail.com>
2026-09-18 15:32:14 +0100

    Create test_checksum.c

---
+++ test-2/test_checksum.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+#include "checksum.h"
+
+void test_empty_data_gives_zero(void) {
+    uint8_t data[] = {};
+    assert(simple_checksum(data, 0) == 0);
+}
+
+void test_known_values_sum_correctly(void) {
+    uint8_t data[] = {1, 2, 3};
+    assert(simple_checksum(data, 3) == 6);
+}
+
+int main(void) {
+    test_empty_data_gives_zero();
+    test_known_values_sum_correctly();
+    printf("All tests passed!\n");
+    return 0;
+}
